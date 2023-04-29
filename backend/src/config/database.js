b6bc0e57c3f77d20d12cdf26ac74e963b0f1954c
Eric Moniz <ericmoniz@outlook.com.br>
2023-04-29 16:21:11 -0300

    aula 130 - Mapeamento das Rotas

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-module.exports = mongoose.connect('mongodb://127.0.0.1:27017/todo', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+module.exports = mongoose
+    .connect('mongodb://127.0.0.1:27017/todo', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch(err => console.error(err));
