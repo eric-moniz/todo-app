87fe6a1a654fa232ff08a96de73819a347dd4eb2
Eric Moniz <ericmoniz@outlook.com.br>
2023-05-04 19:36:37 -0300

    aula 149 - Evento Adicionar (Integração Backend)

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm.jsx';
 import TodoList from './todoList';
 
 // this por padrão aponta para quem chamou a função, para apontar this para a classe Todo,
 // independente de quem chamar a função, usamos a this.'function' = this.'function'.bind(this)
+
+const URL = 'http://localhost:3003/api/todos';
 export default class Todo extends Component {
     constructor(props) {
         super(props);
@@ -19,7 +23,11 @@ export default class Todo extends Component {
     }
 
     handleAdd() {
-        console.log(this.state.description);
+        const description = this.state.description;
+        axios
+            .post(URL, { description })
+            .then(resp => console.log('Funcionou!'))
+            .catch(err => console.log(err));
     }
 
     render() {
