11c53e2a4af37b9e04e4477bf9639068fb8a57b4
Eric Moniz <ericmoniz@outlook.com.br>
2023-05-05 19:46:15 -0300

    aula 154 - Adicionar Teclas de Atalhos

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,36 +2,47 @@ import React from 'react';
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
-const TodoForm = props => (
-    <div role={'form'} className="todoForm">
-        <Grid cols="12 9 10">
-            <input
-                id="description"
-                className="form-control"
-                placeholder="Adicione uma tarefa"
-                onChange={props.handleChange}
-                value={props.description}
-            />
-        </Grid>
+const TodoForm = props => {
+    const keyHandler = e => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd();
+        } else if (e.key === 'Escape') {
+            props.handleClear();
+        }
+    };
 
-        <Grid cols="12 3 2">
-            <IconButton
-                styles={'primary'}
-                icon="plus"
-                onClick={props.handleAdd}
-            />
-            <IconButton
-                styles="info"
-                icon="search"
-                onClick={props.handleSearch}
-            />
-            <IconButton
-                styles="default"
-                icon="close"
-                onClick={props.handleClear}
-            />
-        </Grid>
-    </div>
-);
+    return (
+        <div role={'form'} className="todoForm">
+            <Grid cols="12 9 10">
+                <input
+                    id="description"
+                    className="form-control"
+                    placeholder="Adicione uma tarefa"
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}
+                    value={props.description}
+                />
+            </Grid>
+
+            <Grid cols="12 3 2">
+                <IconButton
+                    styles={'primary'}
+                    icon="plus"
+                    onClick={props.handleAdd}
+                />
+                <IconButton
+                    styles="info"
+                    icon="search"
+                    onClick={props.handleSearch}
+                />
+                <IconButton
+                    styles="default"
+                    icon="close"
+                    onClick={props.handleClear}
+                />
+            </Grid>
+        </div>
+    );
+};
 
 export default TodoForm;
