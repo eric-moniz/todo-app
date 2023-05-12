5f0ab86baaea3c64a395b11d92b184720f5f1ea1
Eric Moniz <ericmoniz@outlook.com.br>
2023-05-12 18:27:44 -0300

    aula 178 - Conectar o TodoForm com Redux

--- frontend-com-redux/src/todo/todoForm.jsx
+++ frontend-com-redux/src/todo/todoForm.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
@@ -45,4 +47,5 @@ const TodoForm = props => {
     );
 };
 
-export default TodoForm;
+const mapStateToProps = state => ({ description: state.todo.description });
+export default connect(mapStateToProps)(TodoForm);
