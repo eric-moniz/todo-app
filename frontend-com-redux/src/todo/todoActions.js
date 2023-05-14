b1a9efc8766c895464b4d172d83b085bd3217ca7
Eric Moniz <ericmoniz@outlook.com.br>
2023-05-14 10:56:37 -0300

    aula 181 - Action Creator de Pesquisa

--- frontend-com-redux/src/todo/todoActions.js
+++ frontend-com-redux/src/todo/todoActions.js
@@ -1,5 +1,21 @@
+import axios from 'axios';
+
+const URL = 'http://localhost:3003/api/todos';
+
 // Action creators
 export const changeDescription = event => ({
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value,
 });
+
+/**
+ * @returns a lista de tarefas em ordem decrescente
+ */
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createdAt`);
+
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request,
+    };
+};
--- frontend-com-redux/src/todo/todoReducer.js
+++ frontend-com-redux/src/todo/todoReducer.js
@@ -23,6 +23,8 @@ export default function todoReducer(state = INITAL_STATE, action) {
     switch (action.type) {
         case 'DESCRIPTION_CHANGED':
             return { ...state, description: action.payload };
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data };
 
         default:
             return state;
